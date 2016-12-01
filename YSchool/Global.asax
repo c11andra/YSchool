757e6d2945fd4ae681ee0648013e9e13d2fe1671
chandra <chandra.bty@gmail.com>
2016-12-01 22:04:40 +0530

    Added modified template
    MVC vs 2015 web template added

---
+++ YSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YSchool.MvcApplication" Language="C#" %>
---
+++ YSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YSchool.Startup))]
+namespace YSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
